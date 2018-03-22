afe1a6a48cc16fdf45ccd4188a495c8dfd296597
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-03-21 21:07:15 -0700

    Reverse Word in place method

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,16 @@
 # A method to reverse each word in a sentence, in place.
-def reverse_words(my_words)
-  raise NotImplementedError
+def reverse_words(array, length)
+  if length == 0
+    return nil
+  end
+
+  i = 0
+  j = length - 1
+  while i < j
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+    i += 1
+    j -= 1
+  end
 end
