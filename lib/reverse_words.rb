2e3c95c6ecad0679ee465504dd7d77f54edb9e5f
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-03-21 21:14:39 -0700

    Current reverse word method, I have it close but it seems to be off by a few spaces. Plan to ask a tutor tomorrow morning.

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,11 +1,11 @@
 # A method to reverse each word in a sentence, in place.
-def reverse_words(array, length)
-  if length == 0
+def reverse_words(array)
+  if array.length == 0
     return nil
   end
-
+  hold = array.length
   i = 0
-  j = length - 1
+  j = hold - 1
   while i < j
     temp = array[i]
     array[i] = array[j]
